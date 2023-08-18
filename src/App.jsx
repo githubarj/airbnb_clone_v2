101bfc95d33331594e9f863097593f502928903a
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-08-18 14:08:52 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Card from "./Components/Card/Card";
+import data from "./Components/Card/data";
 
 function App() {
+  const card = data.map((data) => {
+    return (
+      <Card
+        key={data.id}
+        data = {data}
+      />
+    );
+  });
+
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card />
+      <div className="card-section">{card}</div>
     </div>
   );
 }
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,27 +1,48 @@
 import "./card.css";
+import PropTypes from "prop-types";
 import star from "../../assets/Star 1.png";
 
-function Card() {
+function Card({ data }) {
+  const { title, coverImg, location, rating, reviewCount, price, openSpots } =
+    data;
+  const display =
+    openSpots === 0 ? "SOLD OUT" : location === "Online" ? "ONLINE" : false;
+
   return (
     <div className="card-container">
-      <div className="pic">
-        <button>SOLD OUT</button>
+      <div
+        className="pic"
+        style={{ backgroundImage: `url("src/assets/${coverImg}")` }}
+      >
+        {display && <button>{display}</button>}
       </div>
       <div className="card-content">
         <div className="stats">
           <img src={star} alt="" />
-          <span className="black">5.0</span>
-          <span>(6)</span>
+          <span className="black"> {rating} </span>
+          <span>({reviewCount})</span>
           <span>•</span>
-          <span>USA</span>
+          <span>{location}</span>
         </div>
-        <p>Life lessons with Katie Zaferes</p>
+        <p>{title}</p>
         <p>
-          <span className="bold">From $136</span> / person
+          <span className="bold">From ${price}</span> / person
         </p>
       </div>
     </div>
   );
 }
+
+Card.propTypes = {
+  data: PropTypes.object,
+  state: PropTypes.string,
+  rating: PropTypes.number,
+  reviews: PropTypes.number,
+  country: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
+  image: PropTypes.string,
+  display: PropTypes.bool,
+};
 
 export default Card;
